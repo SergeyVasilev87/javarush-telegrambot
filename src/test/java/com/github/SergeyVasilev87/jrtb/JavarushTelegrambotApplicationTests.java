e8be4f605b0c74e69b44267ace270dfae679c5ff
Sergey Vasilev <vsarussia@mail.ru>
2021-05-15 19:30:15 +0300

    JRTB-0: added SpringBoot skeleton to the project

---
+++ src/test/java/com/github/SergeyVasilev87/jrtb/JavarushTelegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.SergeyVasilev87.jrtb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavarushTelegrambotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
